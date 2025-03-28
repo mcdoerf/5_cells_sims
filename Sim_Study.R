d8faa24377b72c7859f27345440dcec7e5b78ef6
Michael Doerfler <michael.doerfler@emory.edu>
2025-03-27 20:07:19 -0400

    Removed some lines.

--- Sim_Study.R
+++ Sim_Study.R
@@ -31,45 +31,42 @@ population6<-population_generator(Ntot=10000, N_flu=5000, psymp_flu=psymp_flu, p
 
 
 
-#################Simulations.  We let p2 be 0.01, 0.5, 0.1, 0.25, 0.5.
-
-
-sim_study(nsims=1000, population=population1, p1_symp=0.3, p1_nonsymp=0.1, p2=0.01)  ##Letting p2 vary.
+#################Simulations.  We let p2 be 0.5, 0.1, 0.25, 0.5.
+ ##Letting p2 vary.
 sim_study(nsims=1000, population=population1, p1_symp=0.3, p1_nonsymp=0.1, p2=0.05)
 sim_study(nsims=1000, population=population1, p1_symp=0.3, p1_nonsymp=0.1, p2=0.1)
 sim_study(nsims=1000, population=population1, p1_symp=0.3, p1_nonsymp=0.1, p2=0.25)
 sim_study(nsims=1000, population=population1, p1_symp=0.3, p1_nonsymp=0.1, p2=0.5)
 
-
-sim_study(nsims=1000, population=population2, p1_symp=0.3, p1_nonsymp=0.1, p2=0.01)  ##Letting p2 vary.
+  ##Letting p2 vary.
 sim_study(nsims=1000, population=population2, p1_symp=0.3, p1_nonsymp=0.1, p2=0.05)
 sim_study(nsims=1000, population=population2, p1_symp=0.3, p1_nonsymp=0.1, p2=0.1)
 sim_study(nsims=1000, population=population2, p1_symp=0.3, p1_nonsymp=0.1, p2=0.25)
 sim_study(nsims=1000, population=population2, p1_symp=0.3, p1_nonsymp=0.1, p2=0.5)
 
 
-sim_study(nsims=1000, population=population3, p1_symp=0.3, p1_nonsymp=0.1, p2=0.01)  ##Letting p2 vary.
+ ##Letting p2 vary.
 sim_study(nsims=1000, population=population3, p1_symp=0.3, p1_nonsymp=0.1, p2=0.05)
 sim_study(nsims=1000, population=population3, p1_symp=0.3, p1_nonsymp=0.1, p2=0.1)
 sim_study(nsims=1000, population=population3, p1_symp=0.3, p1_nonsymp=0.1, p2=0.25)
 sim_study(nsims=1000, population=population3, p1_symp=0.3, p1_nonsymp=0.1, p2=0.5)
 
 
-sim_study(nsims=1000, population=population4, p1_symp=0.3, p1_nonsymp=0.1, p2=0.01)  ##Letting p2 vary.
+  ##Letting p2 vary.
 sim_study(nsims=1000, population=population4, p1_symp=0.3, p1_nonsymp=0.1, p2=0.05)
 sim_study(nsims=1000, population=population4, p1_symp=0.3, p1_nonsymp=0.1, p2=0.1)
 sim_study(nsims=1000, population=population4, p1_symp=0.3, p1_nonsymp=0.1, p2=0.25)
 sim_study(nsims=1000, population=population4, p1_symp=0.3, p1_nonsymp=0.1, p2=0.5)
 
 
-sim_study(nsims=1000, population=population5, p1_symp=0.3, p1_nonsymp=0.1, p2=0.01)  ##Letting p2 vary.
+ ##Letting p2 vary.
 sim_study(nsims=1000, population=population5, p1_symp=0.3, p1_nonsymp=0.1, p2=0.05)
 sim_study(nsims=1000, population=population5, p1_symp=0.3, p1_nonsymp=0.1, p2=0.1)
 sim_study(nsims=1000, population=population5, p1_symp=0.3, p1_nonsymp=0.1, p2=0.25)
 sim_study(nsims=1000, population=population5, p1_symp=0.3, p1_nonsymp=0.1, p2=0.5)
 
 
-sim_study(nsims=1000, population=population6, p1_symp=0.3, p1_nonsymp=0.1, p2=0.01)  ##Letting p2 vary.
+  ##Letting p2 vary.
 sim_study(nsims=1000, population=population6, p1_symp=0.3, p1_nonsymp=0.1, p2=0.05)
 sim_study(nsims=1000, population=population6, p1_symp=0.3, p1_nonsymp=0.1, p2=0.1)
 sim_study(nsims=1000, population=population6, p1_symp=0.3, p1_nonsymp=0.1, p2=0.25)
